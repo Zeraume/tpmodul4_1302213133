86ef17c842f7cad7df5e46e009b27ae3b60ce366
Zeraume <farissiddiqrp@gmail.com>
2023-03-10 22:47:37 +0700

    menambahkan implementasi dengan table-driven

--- tpmodul4_1302213133/tpmodul4_1302213133/Program.cs
+++ tpmodul4_1302213133/tpmodul4_1302213133/Program.cs
@@ -1,5 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 public class KodePos
 {
-
+    public enum Kelurahan 
+    { 
+        Batununggal, 
+        Kujangsari, 
+        Mengger, 
+        Wates, 
+        Cijaura, 
+        Jatisari, 
+        Margasari, 
+        Sekejati, 
+        Kebonwaru, 
+        Maleer, 
+        Samoja
+    }
+    public static int getKodePos(Kelurahan kelurahan)
+    {
+        int[] isiKodePos = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
+        int inputKodePos = (int)kelurahan;
+        return isiKodePos[inputKodePos];
+    }
+    public static void Main(string[] args)
+    {
+        Kelurahan kelurahan = Kelurahan.Samoja;
+        int kodePos = getKodePos(kelurahan);
+        Console.WriteLine("Kelurahan: " + kelurahan + "\nKode Pos: " + kodePos);
+    }
 }
